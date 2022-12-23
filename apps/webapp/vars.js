dde3d83270ceb9a1a72183ab79d469405304ce1d
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2022-12-22 23:08:38 -0500

    adding missing files

--- apps/webapp/vars.js
+++ apps/webapp/vars.js
@@ -1,3 +1,3 @@
-apiIncrementer = "https://x96gcp9kq1.execute-api.us-west-2.amazonaws.com/kevinc-test";
-apiDBValues = "https://42vxk4kf25.execute-api.us-west-2.amazonaws.com/kevinc-test";
+apiIncrementer = "";
+apiDBValues = "";
 
