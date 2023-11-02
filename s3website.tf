161dbfcb8fa4b76dc9dce180dc9354b4d020f1f8
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2023-11-02 17:25:27 -0400

    update s3 policy and acl

--- s3website.tf
+++ s3website.tf
@@ -3,8 +3,26 @@ resource "aws_s3_bucket" "s3website" {
 }
 
 resource "aws_s3_bucket_acl" "s3website_acl" {
+  bucket     = aws_s3_bucket.s3website.id
+  acl        = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
   bucket = aws_s3_bucket.s3website.id
-  acl    = "public-read"
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.bucket_access_block]
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_access_block" {
+  bucket = aws_s3_bucket.s3website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "s3website_config" {
